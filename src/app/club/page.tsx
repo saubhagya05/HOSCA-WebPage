8333a7c36d8f43e0d1608c09312ec6f4e44e6832
Klaidoscope14 <saagarchaitanya80@gmail.com>
2025-06-10 00:26:57 +0530

    v 2.0.0

--- src/app/club/page.tsx
+++ src/app/club/page.tsx
@@ -6,17 +6,18 @@ import { DotBackgroundDemo } from "@/components/ui/dotbackground";
 
 export default function ClubsPage() {
   return (
-      <div className="container mx-auto px-4">
+    <div className="relative min-h-screen">
+      <div className="relative z-10 container mx-auto px-4">
         <div className="mb-12">
           <h2 className="mt-10 text-3xl md:text-4xl font-bold text-center mb-2 bg-gradient-to-r from-purple-800 via-white to-black bg-clip-text text-transparent animate-gradient-x">
-          <Cover>Cultural Clubs</Cover>
+            <Cover>Cultural Clubs</Cover>
           </h2>
           <p className="text-lg mt-10 text-center mb-8 max-w-3xl mx-auto bg-gradient-to-r from-gray-900 via-orange-600 to-red-700 bg-clip-text text-transparent">
-
             Discover the diverse range of clubs at HOSCA, IIT Patna. Each club offers unique opportunities for students to explore their interests, develop skills, and create lasting memories.
           </p>
         </div>
         <ExpandableCardDemo />
       </div>
+    </div>
   );
-} 
+}
--- src/components/ui/dotbackground.tsx
+++ src/components/ui/dotbackground.tsx
@@ -8,8 +8,8 @@ export function DotBackgroundDemo() {
         className={cn(
           "absolute inset-0",
           "[background-size:20px_20px]",
-          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+          "[background-image:radial-gradient(#000000_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#000000_1px,transparent_1px)]",
         )}
       />
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
